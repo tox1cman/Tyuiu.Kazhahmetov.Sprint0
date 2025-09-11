8fd632edd3c6ad4e58263327303bd27ed561dde1
Таджикович Ержан <kazamircik@gmail.com>
2025-09-11 20:06:06 +0500

    Task3

---
+++ Tyuiu.Kazhahmetov.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.Kazhahmetov.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.Kazhahmetov.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValue()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.Kazhahmetov.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.Kazhahmetov.Sprint0.Task3.V0.Lib;
+
+Console.WriteLine(DataService.Sum(7, 7));
+
