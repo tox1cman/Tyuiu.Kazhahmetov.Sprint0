05619790900c6b7b3fdaea55835ce5c05c0e151d
Таджикович Ержан <kazamircik@gmail.com>
2025-09-11 20:37:23 +0500

    Task4

---
+++ Tyuiu.Kazhahmetov.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.Kazhahmetov.Sprint0.Task4.V0.Lib;
+
+Console.WriteLine(DataService.Addition(1, 5));
+Console.WriteLine(DataService.Subtraction(15, 5));
+Console.WriteLine(DataService.Multiplication(10, 10));
+Console.WriteLine(DataService.Division(5, 5));
+Console.ReadKey();
+
