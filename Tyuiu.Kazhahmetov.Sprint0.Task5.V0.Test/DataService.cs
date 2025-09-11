b97ffa395f78c6ac225565cd5a0bbad35aa7d9d9
Таджикович Ержан <kazamircik@gmail.com>
2025-09-11 20:53:38 +0500

    Task5

---
+++ Tyuiu.Kazhahmetov.Sprint0.Task5.V0.Test/DataService.cs
@@ -0,0 +1,34 @@
+﻿namespace Tyuiu.Kazhahmetov.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static double Division(int a, int b)
+        {
+            if (b==0)
+            {
+                Console.WriteLine("На ноль делить нельзя");
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+
+    }
+}
---
+++ Tyuiu.Kazhahmetov.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.Kazhahmetov.Sprint0.Task5.V0.Lib;
+
+Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+
+Console.WriteLine("A / B = " + DataService.Division(5, 5));
+Console.ReadKey();
