caa3c3e9fad09d54cebfa615184fb9c2512a1260
Таджикович Ержан <kazamircik@gmail.com>
2025-09-12 23:58:00 +0500

    Task6

---
+++ Tyuiu.Kazhahmetov.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.Kazhahmetov.Sprint0.Task6.V0.Lib;
+
+int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+Console.WriteLine("Сумма = " + DataService.AdditionArray(numsArray));
+
+Console.WriteLine("Разность = " + DataService.SubtractionArray(numsArray));
+
+Console.WriteLine("Произведение = " + DataService.MultiplicationArray(numsArray));
+
+Console.ReadKey();
